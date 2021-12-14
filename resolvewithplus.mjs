7d23136fa04f8425cc26689d1b5fed92a41ea40f
chris <chris@bumblehead.com>
2021-12-13 21:01:25 -0800

    convert encoded whitespace to whitespace

--- resolvewithplus.mjs
+++ resolvewithplus.mjs
@@ -7,6 +7,7 @@ const isBuiltinRe = new RegExp(
   '^('+module.builtinModules.join('|').replace('/', '\/')+')$');
 const isDirPathRe = /^\.?\.?(\/|\\)/;
 const isRelPathRe = /^.\.?(?=\/|\\)/;
+const whitespaceRe = /%20/g;
 
 export default (o => {
   o = (requirepath, withpath, opts) => {
@@ -35,12 +36,12 @@ export default (o => {
     var fullpath = null;
 
     withpath = typeof withpath === 'string'
-      ? o.getasdirname(withpath)
+      ? o.getasdirname(withpath.replace(whitespaceRe, ' '))
       : process.cwd();
 
-    if (o.iscoremodule(requirepath)) {
+    if (isBuiltinRe.test(requirepath)) {
       fullpath = requirepath;
-    } else if (o.isdirpath(requirepath)) {
+    } else if (isDirPathRe.test(requirepath)) {
       fullpath = o.getasfileordir(requirepath, withpath, opts);
     } else {
       fullpath = o.getasnode_module(requirepath, withpath, opts);
@@ -48,12 +49,6 @@ export default (o => {
 
     return fullpath;
   };  
-
-  o.isdirpath = p => isDirPathRe.test(p);
-
-  o.isrelpath = p => isRelPathRe.test(p);
-
-  o.iscoremodule = p => isBuiltinRe.test(p);
 
   o.isfilesync = (file, stat) => {
     try {
@@ -147,7 +142,7 @@ export default (o => {
   };
 
   o.getasfileordir = (requirepath, withpath, opts) => {
-    const temppath = o.isrelpath(requirepath)
+    const temppath = isRelPathRe.test(requirepath)
       ? path.join(withpath, requirepath)
       : requirepath;
 
