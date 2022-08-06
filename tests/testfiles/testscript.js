6c2e41e1979be9fc00db3314d936b0d0bdc8e22b
chris <chris@bumblehead.com>
2022-08-06 13:02:12 -0700

    restore testscript to previous master state

--- tests/testfiles/testscript.js
+++ tests/testfiles/testscript.js
@@ -1,28 +1,8 @@
-import libindex1 from 'nodejsexample_01_exports';
-import libindex2 from 'nodejsexample_01_exports/lib';
-import libindex3 from 'nodejsexample_01_exports/lib/index';
-import libindex4 from 'nodejsexample_01_exports/lib/index.js';
+// Filename: testscript.js  
+// Timestamp: 2015.07.15-12:04:02 (last modified)
+// Author(s): bumblehead <chris@bumblehead.com>
 
-import featureindex1 from 'nodejsexample_01_exports/feature';
-import featureindex2 from 'nodejsexample_01_exports/feature/index';
-import featureindex3 from 'nodejsexample_01_exports/feature/index.js';
+const modulea = require('./path/to/indexfile');
+const moduleb = require('testmodule');
 
-const libindex1wrap = () => libindex1
-const libindex2wrap = () => libindex2
-const libindex3wrap = () => libindex3
-const libindex4wrap = () => libindex4
-
-const featureindex1wrap = () => featureindex1
-const featureindex2wrap = () => featureindex2
-const featureindex3wrap = () => featureindex3
-
-export {
-  libindex1wrap,
-  libindex2wrap,
-  libindex3wrap,
-  libindex4wrap,
-
-  featureindex1wrap,
-  featureindex2wrap,
-  featureindex3wrap
-};
+module.exports = { modulea, moduleb };
