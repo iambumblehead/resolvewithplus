23a7994241c7659c5f677a21e7d0b60f0ac76ffd
chris <chris@bumblehead.com>
2022-08-06 13:04:45 -0700

    added comment above test

--- tests/tests-basic/tests-export-patterns.test.js
+++ tests/tests-basic/tests-export-patterns.test.js
@@ -3,6 +3,20 @@ import test from 'node:test'
 import assert from 'node:assert/strict'
 import resolvewithplus from 'resolvewithplus';
 
+// from: https://nodejs.org/api/packages.html#package-entry-points
+// {
+//   "name": "my-package",
+//   "exports": {
+//     ".": "./lib/index.js",
+//     "./lib": "./lib/index.js",
+//     "./lib/index": "./lib/index.js",
+//     "./lib/index.js": "./lib/index.js",
+//     "./feature": "./feature/index.js",
+//     "./feature/index": "./feature/index.js",
+//     "./feature/index.js": "./feature/index.js",
+//     "./package.json": "./package.json"
+//   }
+// }
 test('should mock all exports from nodejsexample_01_exports', () => {
   const noderesolvedlibindex = path
     .resolve('./nodejsexample_01_exports/lib/index.js');
